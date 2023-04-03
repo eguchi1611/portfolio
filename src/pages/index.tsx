79f9243dbddb7ca751fa6dfd47606e4fffbbd0ca
ITO Keita <kk79it@gmail.com>
2023-04-03 19:48:47 +0900

    仮ページを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
 export default function IndexPage() {
-  return <div className="text-4xl font-bold text-red-400">Index Page</div>;
+  return (
+    <div className="pt-16 text-center text-4xl font-bold text-teal-400">
+      作成中
+    </div>
+  );
 }
