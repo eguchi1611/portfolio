60c7ae10495af2b1a7572e1604f1e66f43426a1c
ITO Keita <kk79it@gmail.com>
2023-04-08 13:58:21 +0900

    タイトルにリンクを設定

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,10 +1,11 @@
 import SocialCard from "./social-card/social-card";
+import Link from "next/link";
 
 export default function Profile() {
   return (
     <>
       <div className="text-3xl font-bold tracking-widest text-teal-500">
-        Keita ITO
+        <Link href="/">Keita ITO</Link>
       </div>
       <div className="mt-2 text-neutral-700">
         趣味でプログラミングしています
