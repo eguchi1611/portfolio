2eb50669944abd4c2841b52c0e8701567bc2e856
ITO Keita <kk79it@gmail.com>
2023-04-13 19:24:25 +0900

    コンポーネント構成の変更

---
+++ src/components/site-title.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function SiteTitle() {
+  return (
+    <div className="font-mono text-3xl font-extrabold tracking-widest text-teal-500">
+      <Link href="/">Keita ITO</Link>
+    </div>
+  );
+}
