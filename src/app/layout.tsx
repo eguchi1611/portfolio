fed0dc164e80894ffcf53240bc1bbc50a0470877
Keita Ito <eguchi@eguchi.cc>
2023-11-28 14:19:09 +0000

    フォントの変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
+import { Noto_Sans_JP } from "next/font/google";
 import { PropsWithChildren } from "react";
 import "./globals.css";
 
+const noto_sans_jp = Noto_Sans_JP({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
-  title: "Portfolio",
+  title: "Eguchi's Portfolio",
   description: "Eguchi's Portfolio",
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body className={noto_sans_jp.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function HomePage() {
   return (
     <div>
-      <p className="text-pink-400 text-4xl tracking-wider text-center">Home</p>
+      <p className="text-pink-400 text-4xl tracking-wider text-center">
+        あいうえお
+      </p>
     </div>
   );
 }
