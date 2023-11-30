8726b6d868a071d70a92c53395c767376a0384ec
Keita Ito <eguchi@eguchi.cc>
2023-11-30 12:05:09 +0900

    全削除

--- src/app/blog/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export default function BlogLayout({ children }: PropsWithChildren) {
-  return (
-    <div className="prose prose-slate max-w-xl mx-auto dark:prose-invert">
-      {children}
-    </div>
-  );
-}
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <p className="text-center text-4xl tracking-wider text-pink-400">
-        あいうえお
-      </p>
-    </div>
-  );
-}
