60cae5193331c7036943472e612580255573cc5c
Keita Ito <eguchi@eguchi.cc>
2023-11-30 03:49:18 +0000

    prettier-plugin-tailwindcssの構成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div>
-      <p className="text-pink-400 font-bold text-3xl">Hello World</p>
+      <p className="text-3xl font-bold text-pink-400">Hello World</p>
     </div>
   );
 }
